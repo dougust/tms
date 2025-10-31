6d7633b3cb8aa5f6b7ae602cf25717dc41b2c5ea
gustavoch <gustavo.francelino@gmail.com>
2025-10-31 18:22:01 -0300

    fix dep issue

--- apps/dougust/src/modules/pessoas-juridicas/pessoas-juridicas.module.ts
+++ apps/dougust/src/modules/pessoas-juridicas/pessoas-juridicas.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { PessoasJuridicasService } from './pessoas-juridicas.service';
 import { PessoasJuridicasController } from './pessoas-juridicas.controller';
 import { DrizzleModule } from '../database/database.module';
+import { UserContextModule } from '../../common/user-context/user-context.module';
 
 @Module({
-  imports: [DrizzleModule],
+  imports: [DrizzleModule, UserContextModule],
   controllers: [PessoasJuridicasController],
   providers: [PessoasJuridicasService],
   exports: [PessoasJuridicasService],
