f16837ac318e611e5051bc0499daf7f72f727f1b
gustavoch <gustavo.francelino@gmail.com>
2025-11-03 15:51:45 -0300

    rename be e2e project

--- apps/be-e2e/jest.config.ts
+++ apps/be-e2e/jest.config.ts
@@ -1,5 +1,5 @@
 export default {
-  displayName: 'dougust-e2e',
+  displayName: 'be-e2e',
   preset: '../../jest.preset.js',
   globalSetup: '<rootDir>/src/support/global-setup.ts',
   globalTeardown: '<rootDir>/src/support/global-teardown.ts',
