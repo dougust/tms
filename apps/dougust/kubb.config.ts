13f18cb59a4c707bb233cf46cda658202f63f33c
gustavoch <gustavo.francelino@gmail.com>
2025-11-03 14:08:16 -0300

    autogenerate clients

--- apps/dougust/kubb.config.ts
+++ apps/dougust/kubb.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from '@kubb/core';
 import { pluginOas } from '@kubb/plugin-oas';
 import { pluginReactQuery } from '@kubb/plugin-react-query';
 import { pluginTs } from '@kubb/plugin-ts';
+import { pluginZod } from '@kubb/plugin-zod';
 
 export default defineConfig(() => {
   return {
@@ -17,10 +18,15 @@ export default defineConfig(() => {
     plugins: [
       pluginOas({ validate: false }),
       pluginTs(),
+      pluginZod(),
       pluginReactQuery({
         suspense: false,
+        group: {
+          type: 'tag',
+          name: ({ group }) => `${group}`,
+        },
         client: {
-          importPath: '../../client',
+          importPath: '../../../client',
         },
       }),
     ],
--- libs/autogen-clients/src/autogen/types/index.ts
+++ libs/autogen-clients/src/autogen/types/index.ts
@@ -1,40 +1,40 @@
-export type { CreatePessoaJuridicaDto } from './CreatePessoaJuridicaDto.ts';
+export type { CadastroDto } from './CadastroDto.ts';
+export type { CreateFuncionarioDto } from './CreateFuncionarioDto.ts';
+export type { FuncionarioDto } from './FuncionarioDto.ts';
+export type {
+  FuncionariosControllerCreate201,
+  FuncionariosControllerCreateMutationRequest,
+  FuncionariosControllerCreateMutationResponse,
+  FuncionariosControllerCreateMutation,
+} from './FuncionariosControllerCreate.ts';
+export type {
+  FuncionariosControllerFindAll200,
+  FuncionariosControllerFindAllQueryResponse,
+  FuncionariosControllerFindAllQuery,
+} from './FuncionariosControllerFindAll.ts';
+export type {
+  FuncionariosControllerFindOnePathParams,
+  FuncionariosControllerFindOne200,
+  FuncionariosControllerFindOneQueryResponse,
+  FuncionariosControllerFindOneQuery,
+} from './FuncionariosControllerFindOne.ts';
+export type {
+  FuncionariosControllerRemovePathParams,
+  FuncionariosControllerRemove200,
+  FuncionariosControllerRemoveMutationResponse,
+  FuncionariosControllerRemoveMutation,
+} from './FuncionariosControllerRemove.ts';
+export type {
+  FuncionariosControllerUpdatePathParams,
+  FuncionariosControllerUpdate200,
+  FuncionariosControllerUpdateMutationRequest,
+  FuncionariosControllerUpdateMutationResponse,
+  FuncionariosControllerUpdateMutation,
+} from './FuncionariosControllerUpdate.ts';
+export type { GetFuncionarioResponseDto } from './GetFuncionarioResponseDto.ts';
 export type {
   HealthControllerGetHealth200,
   HealthControllerGetHealthQueryResponse,
   HealthControllerGetHealthQuery,
 } from './HealthControllerGetHealth.ts';
-export type {
-  PessoasJuridicasControllerCreate201,
-  PessoasJuridicasControllerCreateMutationRequest,
-  PessoasJuridicasControllerCreateMutationResponse,
-  PessoasJuridicasControllerCreateMutation,
-} from './PessoasJuridicasControllerCreate.ts';
-export type {
-  PessoasJuridicasControllerFindAllQueryParams,
-  PessoasJuridicasControllerFindAll200,
-  PessoasJuridicasControllerFindAllQueryResponse,
-  PessoasJuridicasControllerFindAllQuery,
-} from './PessoasJuridicasControllerFindAll.ts';
-export type {
-  PessoasJuridicasControllerFindOnePathParams,
-  PessoasJuridicasControllerFindOneQueryParams,
-  PessoasJuridicasControllerFindOne200,
-  PessoasJuridicasControllerFindOneQueryResponse,
-  PessoasJuridicasControllerFindOneQuery,
-} from './PessoasJuridicasControllerFindOne.ts';
-export type {
-  PessoasJuridicasControllerRemovePathParams,
-  PessoasJuridicasControllerRemoveQueryParams,
-  PessoasJuridicasControllerRemove200,
-  PessoasJuridicasControllerRemoveMutationResponse,
-  PessoasJuridicasControllerRemoveMutation,
-} from './PessoasJuridicasControllerRemove.ts';
-export type {
-  PessoasJuridicasControllerUpdatePathParams,
-  PessoasJuridicasControllerUpdate200,
-  PessoasJuridicasControllerUpdateMutationRequest,
-  PessoasJuridicasControllerUpdateMutationResponse,
-  PessoasJuridicasControllerUpdateMutation,
-} from './PessoasJuridicasControllerUpdate.ts';
-export type { UpdatePessoaJuridicaDto } from './UpdatePessoaJuridicaDto.ts';
+export type { UpdateFuncionarioDto } from './UpdateFuncionarioDto.ts';
